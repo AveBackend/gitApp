81206b2fa1e477578a018da713554067143a9601
Kir <tehhen@bk.ru>
2023-07-01 16:41:30 +0300

    Initial Commit

---
+++ gitApp/gitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitAppApp.swift
+//  gitApp
+//
+//  Created by Kir on 01.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct gitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
